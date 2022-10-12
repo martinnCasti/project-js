8f2caa05f0c7e16010ebda14ff7471faf3c7af3f
martinnCasti <mj.castineira@gmail.com>
2022-10-12 20:28:01 -0300

    fetch local

--- home.js
+++ home.js
@@ -20,77 +20,99 @@ let contenedor = document.getElementById("contenedor");
 let formFila = document.getElementById("Registro");
 
 
-formFila.addEventListener("submit", (e)=>{
-    e.preventDefault();
-    filaVirtual.innerHTML = "";
+// formFila.addEventListener("submit", (e)=>{
+//     e.preventDefault();
+//     filaVirtual.innerHTML = "";
 
-    if(nombre.value == "" || apellido.value == "" || cantComensales.value == ""){
-        let mensaje = "No es posible cargar la reserva, intenta de nuevo";
-        alert(mensaje);
+//     if(nombre.value == "" || apellido.value == "" || cantComensales.value == ""){
+//         let mensaje = "No es posible cargar la reserva, intenta de nuevo";
+//         alert(mensaje);
         
-    }else{
-        //genero las reservas para cada cliente
-    let reservaCliente = new Cliente(nombre.value,apellido.value,cantComensales.value);
+//     }else{
+//         //genero las reservas para cada cliente
+//     let reservaCliente = new Cliente(nombre.value,apellido.value,cantComensales.value);
 
-    //inserto el objeto en el array de reservas
-    reservas.push(reservaCliente);
+//     //inserto el objeto en el array de reservas
+//     reservas.push(reservaCliente);
 
-    //guardo el el localstorage los objetos
-    localStorage.setItem("cliente", JSON.stringify(reservas))
+//     //guardo el el localstorage los objetos
+//     localStorage.setItem("cliente", JSON.stringify(reservas))
 
-    let reservasGeneradas = [];
-    let reservaStorage = JSON.parse(localStorage.getItem("cliente"))
-
-    
-    reservaStorage ? reservasGeneradas = reservaStorage : console.log("No hay datos en el localstorage");
+//     let reservasGeneradas = [];
+//     let reservaStorage = JSON.parse(localStorage.getItem("cliente"))
 
     
-    reservaStorage.forEach(element => {
-    
-    let div = document.createElement("div");
+//     reservaStorage ? reservasGeneradas = reservaStorage : console.log("No hay datos en el localstorage");
 
-    div.innerHTML =` 
-    <h2>Reservas Generadas</h2>
-    <h3>Nombre y apellido: ${element.nombre}, ${element.apellido}</h3>
-    <h3>Cantidad de Comensales: ${element.comensales}</h3>
     
-    `;
-    filaVirtual.append(div);
-    Swal.fire(
-        'Perfecto!',
-        'Se ha guardado la reserva!',
-        'success'
-    )
-    });
+//     reservaStorage.forEach(element => {
+    
+//     let div = document.createElement("div");
+
+//     div.innerHTML =` 
+//     <h2>Reservas Generadas</h2>
+//     <h3>Nombre y apellido: ${element.nombre}, ${element.apellido}</h3>
+//     <h3>Cantidad de Comensales: ${element.comensales}</h3>
+    
+//     `;
+//     filaVirtual.append(div);
+//     Swal.fire(
+//         'Perfecto!',
+//         'Se ha guardado la reserva!',
+//         'success'
+//     )
+//     });
     
 
-    console.log(reservaStorage);
+//     console.log(reservaStorage);
 
-    }
-})
+//     }
+// })
 
 let botonEliminar = document.getElementById("btnEliminar")
 
 botonEliminar.addEventListener("click", () =>{
+    fetch("./data/data.json")
+    .then(response => response.json())
+    .then(data =>{
+        data.forEach((element) => {
+            let div = document.createElement("div");
+            div.innerHTML =`
+            <h2>Nro: ${element.id}
+            <h3>Nombre y apellido: ${element.nombreApellido}
+            <h3>Cantidad de comensales: ${element.CantComensales}
+            <hr/>
+            
+            `;
+    
+            contenedor.append(div);    
+        });
 
-    Swal.fire({
-        title: 'Seguro que desea eliminar la fila generada?',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Si, deseo eliminarla'
-        }).then((result) => {
-        if (result.isConfirmed) {
-            localStorage.clear();
-            filaVirtual.innerHTML= " ";
-            Swal.fire(
-            'Elimnada!',
-            'La fila ha sido eliminada',
-            'success'
-        )
-        }
-    })  
+    });
+
+
+
+
+
+
+    // Swal.fire({
+    //     title: 'Seguro que desea eliminar la fila generada?',
+    //     icon: 'warning',
+    //     showCancelButton: true,
+    //     confirmButtonColor: '#3085d6',
+    //     cancelButtonColor: '#d33',
+    //     confirmButtonText: 'Si, deseo eliminarla'
+    //     }).then((result) => {
+    //     if (result.isConfirmed) {
+    //         localStorage.clear();
+    //         filaVirtual.innerHTML= " ";
+    //         Swal.fire(
+    //         'Elimnada!',
+    //         'La fila ha sido eliminada',
+    //         'success'
+    //     )
+    //     }
+    // })  
 })
 
 
